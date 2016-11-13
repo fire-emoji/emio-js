525619b09ab55fb8311238028333f1ed8475c83d
KadynCBR <kadyn_martinez@hotmail.com>
2016-11-12 16:25:36 -0800

    Updated routes

--- index.js
+++ index.js
@@ -1,20 +1,75 @@
 var express = require('express');
+var Client = require('node-rest-client').Client;
+
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+function toEmioData(data) {
+  return {
+    "Text": "Taco!",
+    "Location": {
+      "Longitude": data[0],
+      "Latitude" : data[1],
+    },
+    "Emotion" : data[2]
+  }
+}
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+/**
+ *  HTTP get /emotions
+ */
+app.get('/emotions', function(request, response) {
+  response.send('This works!');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/emotions/area/:tags', function(request, response) {
+  var tags = request.params.tags.split(',');
+  if (tags.length == 4) {
+    // Appropriate tag.
+    // Handle Appropriately.
+   j = toEmioData(tags)
+   response.send(JSON.stringify(j));
+  } else {
+    response.send(404);
+  }
+});
+
+app.get('/emotions/area/:tags/keyword/:keyword', function(request, response) {
+  var tags = request.params.tags.split(',');
+  if (tags.length == 4) {
+   //Send keywords
+   j = toEmioData(tags);
+   response.send(JSON.stringify(j));
+  } else {
+    respond.send(404);
+  }
+});
+
+app.get('/emotions/area/:tags/emotion/:emotion', function(request, response) {
+  var tags = request.params.tags.split(',');
+  if (tags.length == 4) {
+   //Send keywords
+   j = toEmioData(tags);
+   j.emotion = request.params.emotion;
+   response.send(JSON.stringify(j));
+  } else {
+    respond.send(404);
+  }
+});
+
+app.get('/amadeus/origin/:origin/max_price/:max_price', function(request, response) {
+  var client = new Client();
+  var j;
+  var apiKey = 'RvgLqBzb1H67WTUb4WQbKKQPgmk9SAlK';
+  var reqstring = "http://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?origin=" +
+                  request.params.origin + "&max_price=" +
+                  request.params.max_price + "&apikey=" +
+                  apiKey;
+  client.get(reqstring, function(data, res) {
+    // console.log("Data: ");
+    // console.log(data);
+    response.send(data);
+  });
 });
 
 
+app.listen(3000);
